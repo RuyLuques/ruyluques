3d9038e8697c0663793eaef97166b9882ef6e802
Ana Ruy <ruyluques@hotmail.com>
2026-01-10 18:55:08 -0300

    Ajustes na responsividade, titulo e texto no componente home

--- app/sections/home/Home.tsx
+++ app/sections/home/Home.tsx
@@ -1,47 +1,40 @@
 "use client";
 
-import { useState } from "react";
 import ThemeToggle from "../../components/ui/ThemeToggle";
 import Technologies from "../../components/shared/Technologies";
 import Education from "../education/Education";
 
 export default function Home() {
-    const [openAbout, setOpenAbout] = useState(false);
-
     return (
         <>
             <section
                 id="home"
                 className="
-            relative
-            mx-auto
-            flex
-            max-w-7xl
-            flex-col
-            items-center
-            gap-10
-            px-6
-            pt-40
-            pb-32
-            text-center
-            "
+          relative
+          mx-auto
+          flex
+          max-w-7xl
+          flex-col
+          items-center
+          gap-8
+          px-4
+          sm:px-6
+          lg:px-8
+          pt-32
+          sm:pt-40
+          pb-24
+          sm:pb-32
+          text-center
+        "
             >
-                <div className="absolute right-6 top-6">
+                <div className="absolute right-4 top-4 sm:right-6 sm:top-6">
                     <ThemeToggle />
                 </div>
-
-                <h1 className="max-w-3xl text-4xl font-bold tracking-tight sm:text-5xl">
-                    Ana Ruy Luques
-                </h1>
-
-                <p className="max-w-xl text-base">
-                    Analista de sistemas formada em <strong>análise e desenvolvimento de sistemas</strong>,
-                    pós-graduanda em <strong>dados</strong> e <strong>automação com IA</strong>.
-                    Mais de <strong>4 anos</strong> de experiência em <strong> operações e processos</strong>.
-                    Estou me especializando em <strong>dados</strong> e <strong>automação</strong>.
-                </p>
-
-                <Technologies />
+                <h1 className="max-w-3xl text-4xl font-bold tracking-tight sm:text-5xl"> Ana Ruy Luques </h1>
+                <p className="max-w-xl text-base"> Analista de sistemas formada em <strong>análise e desenvolvimento de sistemas</strong>, pós-graduanda em <strong>dados</strong> e <strong>automação com IA</strong>. Mais de <strong>4 anos</strong> de experiência em <strong> operações e processos</strong>. Estou me especializando em <strong>dados</strong> e <strong>automação</strong>. </p>
+                <div className="w-full flex justify-center mt-4">
+                    <Technologies />
+                </div>
             </section>
             <Education />
         </>
