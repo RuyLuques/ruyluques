88d5c2815f55eb47feb6eeae82915d3fa312167a
Ana Ruy <ruyluques@hotmail.com>
2026-02-01 15:59:37 -0300

    Atualização no turno da minha faculdade

--- app/data/education.ts
+++ app/data/education.ts
@@ -13,8 +13,8 @@ export interface Course {
 
 export const courses: Course[] = [
   { name: "CNH 'B'", institution: "Detran", type: "Carteira de Habilitação", status: "Cursando" },
-  { name: "Ciência de Dados", institution: "Faculdade Líbano", details: "Noite", type: "Pós-graduação", status: "Cursando", schedule: "Noite", finish: "2026" },
-  { name: "Robótica e Inteligência Artificial", institution: "Faculdade Líbano", details: "Noite", type: "Pós-graduação", status: "Cursando", schedule: "Noite", finish: "2026" },
+  { name: "Ciência de Dados", institution: "Faculdade Líbano", details: "EAD", type: "Pós-graduação", status: "Cursando", schedule: "Noite", finish: "2026" },
+  { name: "Robótica e Inteligência Artificial", institution: "Faculdade Líbano", details: "EAD", type: "Pós-graduação", status: "Cursando", schedule: "Noite", finish: "2026" },
   { name: "Análise e Desenvolvimento de Sistemas", institution: "UNICV", details: "EAD", type: "Graduação", status: "Concluído", finish: "2025" },
   { name: "Marketing Digital", institution: "UNICV", details: "EAD", type: "Graduação", status: "Concluído", finish: "2023" },
   { name: "AWS", institution: "Escola da Nuvem", details: "EAD", type: "Complementar", status: "Concluído", finish: "2023" },
