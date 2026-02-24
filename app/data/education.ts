b55ca3bdb54b57657a20dd76c1dc0c9f995f1512
Ana Ruy <ruyluques@hotmail.com>
2026-02-24 11:35:14 -0300

    Troca no tipo CourseType

--- app/data/education.ts
+++ app/data/education.ts
@@ -1,5 +1,5 @@
 export type CourseStatus = "Concluído" | "Cursando";
-export type CourseType = "Graduação" | "Pós-graduação" | "Detran" | "Complementar" | "Carteira de Habilitação" | "Certificação";
+export type CourseType = "Graduação" | "Pós-graduação" | "Tecnólogo" | "Complementar" | "Carteira de Habilitação" | "Certificação";
 
 export interface Course {
   name: string;
@@ -13,8 +13,8 @@ export interface Course {
 
 export const courses: Course[] = [
   { name: "Engenharia de Software", institution: "UNICV", details: "EAD", type: "Graduação", status: "Cursando", finish: "2029" },
-  { name: "Análise e Desenvolvimento de Sistemas", institution: "UNICV", details: "EAD", type: "Graduação", status: "Concluído", finish: "2025" },
-  { name: "Marketing Digital", institution: "UNICV", details: "EAD", type: "Graduação", status: "Concluído", finish: "2023" },
+  { name: "Análise e Desenvolvimento de Sistemas", institution: "UNICV", details: "EAD", type: "Tecnólogo", status: "Concluído", finish: "2025" },
+  { name: "Marketing Digital", institution: "UNICV", details: "EAD", type: "Tecnólogo", status: "Concluído", finish: "2023" },
   { name: "AWS", institution: "Escola da Nuvem", details: "EAD", type: "Complementar", status: "Concluído", finish: "2023" },
   { name: "Desenvolvedor Web", institution: "SENAI - Vila Leopoldina", details: "Semipresencial", type: "Complementar", status: "Concluído", finish: "2022" },
   { name: "Java Fullstack", institution: "Generation Brasil", details: "Semipresencial", type: "Complementar", status: "Concluído", finish: "2022" },
